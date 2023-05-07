1e57ea9c7786ccf554f1e44fba9ba3e9602648b6
Elena Strakhova <elenastrakhova@gmail.com>
2023-05-07 18:25:19 -0400

    timer tinkering

--- src/App.js
+++ src/App.js
@@ -61,17 +61,19 @@ function App() {
     const today = new Date();
     if (new Date().getDate() <= 14) {
       todate = new Date(Date.UTC(todayYear, todayMonth, 1, todayMonth % 2 === 1 ? 19 : 7, 0, 0, 0));
-      console.log('1 ' + todayMonth);
-      if (today < new Date(todate)) {
-        console.log((today < new Date(todate)));
+      // console.log('1 ' + todayMonth + " " + today.getTime() + " " + todate.getTime());
+      if (new Date(todate) < today) {
+        // console.log('2 ' + (today < new Date(todate)));
         todate = new Date(Date.UTC(todayYear, todayMonth, 1, todayMonth % 2 === 1 ? 19 : 7, 0, 0, 0));
-        console.log('2 ' + todate);
-      }
-      if (today > new Date(todate)) {
-        console.log((today > new Date(todate)));
-        // todayMonth = todayMonth + 1;
+        // console.log('2 ' + todate);
+      } 
+      // else
+      if (new Date(todate) > today)
+       {
+        // console.log('3 ' + (today > new Date(todate)));
+        todayMonth = todayMonth + 1;
         todate = new Date(Date.UTC(todayYear, todayMonth, 1, todayMonth % 2 === 1 ? 19 : 7, 0, 0, 0));
-        console.log('3 ' + todate);
+        // console.log('3 ' + todate);
       }
     }
     if (new Date().getDate() > 14) {
