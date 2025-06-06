686dd8e494221bf3e954e5191f3db8b455ce1b5a
Elena Strakhova <elenastrakhova@gmail.com>
2025-06-06 00:24:28 -0400

    fallback russian font 2

--- src/App.js
+++ src/App.js
@@ -183,7 +183,7 @@ function App() {
                   <img src={cover} alt={name} />
                   <figcaption className={theme}>{month}</figcaption>
                 </figure>
-                <p style={author === "Stephenie Meyer" ? {"font-family": "Twilight"} : {"font-family": "Acme"}}>
+                <p style={author === "Stephenie Meyer" ? {"font-family": "Twilight"} : {"font-family": "'Acme', 'Balsamiq Sans'"}}>
                     "{name}"
                 </p>
                 <p className="author">{author}</p>
