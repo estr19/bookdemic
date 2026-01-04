78ec20f07ec0eb3ed53aab2b427322e347d815f1
Elena Strakhova <elenastrakhova@gmail.com>
2026-01-04 18:25:13 -0500

    fixed countdown

--- src/App.js
+++ src/App.js
@@ -59,19 +59,18 @@ function App() {
     let timeObjects = [];
     const today = new Date();
     if (new Date().getDate() <= 14) {
-      todate = new Date(Date.UTC(todayYear, todayMonth, 1, todayMonth % 2 === 1 ? 19 : 4, 0, 0, 0));
+      todate = new Date(Date.UTC(todayYear, todayMonth, 1, 19, 0, 0, 0));
       // console.log('1 ' + todayMonth + " " + today.getTime() + " " + todate.getTime());
       if (new Date(todate) > today) {
-        console.log('2 ' + (today < new Date(todate)));
-        todate = new Date(Date.UTC(todayYear, todayMonth, 1, todayMonth % 2 === 1 ? 19 : 4, 0, 0, 0));
+        todate = new Date(Date.UTC(todayYear, todayMonth, 1, 19, 0, 0, 0));
         // console.log('2 ' + todate);
       } 
       else
       if (new Date(todate) > today)
        {
-        // console.log('3 ' + (today < new Date(todate)));
+        console.log('3 ' + (today < new Date (todate)));
         todayMonth = todayMonth + 1;
-        todate = new Date(Date.UTC(todayYear, todayMonth, 1, todayMonth % 2 === 1 ? 19 : 4, 0, 0, 0));
+        todate = new Date(Date.UTC(todayYear, todayMonth, 1, 19, 0, 0, 0));
         // console.log('3 ' + todate);
       }
     }
@@ -79,10 +78,10 @@ function App() {
       todayMonth = todayMonth + 1;
       // console.log('4 ' + todate);
     }
-    if (new Date().getDate() > 14) todate = new Date(Date.UTC(todayYear, todayMonth + 1, 1, todayMonth % 2 === 1 ? 19 : 4, 0, 0, 0));
-    todate = new Date(Date.UTC(todayYear, todayMonth, 1, todayMonth % 2 === 1 ? 19 : 4, 0, 0, 0));
+    if (new Date().getDate() > 14) todate = new Date(Date.UTC(todayYear, todayMonth + 1, 1, 19, 0, 0, 0));
+    todate = new Date(Date.UTC(todayYear, todayMonth, 1, 19, 0, 0, 0));
     todate.setDate(14 - todate.getDay());
-    console.log('5 ' + todate);
+    // console.log('5 ' + todate);
     setShowDate(todate);
     setLoading(false);
 
